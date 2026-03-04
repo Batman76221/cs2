56e7197236bf257972d2fc879641ab404fcb4782
atkbatman72-lgtm <atkbatman72@gmail.com>
2026-03-05 00:36:50 +0100

    Add files via upload

--- output/engine2_dll.rs
+++ output/engine2_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-26 00:07:53.493015100 UTC
+// 2026-03-04 23:29:02.969486 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- output/rendersystemdx11_dll.cs
+++ output/rendersystemdx11_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-26 00:07:53.493015100 UTC
+// 2026-03-04 23:29:02.969486 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: rendersystemdx11.dll
